2d81c55d9ae47d2458250f70a844ed903ce99f98
manviiiichawlaa <manviiiii@Manvis-MacBook-Air.local>
2025-11-02 14:50:51 +0530

    Updated files

---
+++ assignmentques13.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int n,i;
+
+    printf("enter the number of elements:");
+    scanf("%d",&n);
+    int a[n];
+    printf("enter %d elements:\n",n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if((i==0||a[i]>=a[i-1])&&
+        (i==n-1||a[i]>=a[i+1]))
+    {
+        printf("peak element is %d\n",a[i]);
+      }
+    }
+    return 0;
+}
